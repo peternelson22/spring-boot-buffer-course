b1b418d342cb0070e012df2af0bb819178175bc0
PeteNelson22 <peternelson063@gmail.com>
2023-01-02 11:55:44 +0100

    initial commit

---
+++ src/main/java/com/nelson/microservices/SpringBootMicroservicesCourseApplication.java
@@ -0,0 +1,13 @@
+package com.nelson.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroservicesCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroservicesCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nelson/microservices/SpringBootMicroservicesCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nelson.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroservicesCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
